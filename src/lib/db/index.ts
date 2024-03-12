78e71b5a586c991688b4dc67e9278caf2d91f584
Aviral <soniaviral14@gmail.com>
2024-03-13 00:09:52 +0530

    homepage design and db setup

---
+++ src/lib/db/index.ts
@@ -0,0 +1,14 @@
+import { neon, neonConfig } from "@neondatabase/serverless";
+import type { NeonQueryFunction } from "@neondatabase/serverless";
+import { drizzle } from "drizzle-orm/neon-http";
+
+neonConfig.fetchConnectionCache = true;
+
+if (!process.env.DATABASE_URL) {
+  throw new Error("database url not found");
+}
+
+
+const sql: NeonQueryFunction<boolean, boolean> = neon(process.env.DATABASE_URL);
+
+export const DB = drizzle(sql);
