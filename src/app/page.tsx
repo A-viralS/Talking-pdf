95a64eb532c627d2b43804805f58843229575496
Aviral <soniaviral14@gmail.com>
2024-03-19 11:49:19 +0530

    lil ui update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -20,17 +20,20 @@ export default async function Home() {
 
   const isAuth = !!userId;
   return (
+
     <div className="w-sreen min-h-screen bg-gradient-to-r from-rose-100 to-teal-100">
+      <div className=" absolute right-0 p-3 ">   <UserButton afterSignOutUrl="/" >signout</UserButton></div>
+
       <div className="absolute top-1/2 left-1/2 -translate-x-1/2  -translate-y-1/2 ">
         <div className="fl
         ex flex-col items-center text-center">
-          <div className="flex items-center">
+          <div className="flex items-center justify-center">
 
             <h1 className="mr-3 text-5xl font-semibold">Chat with any PDF</h1>
 
-            <UserButton afterSignOutUrl="/">signout</UserButton>
+
           </div>
-          <div className="flex mt-2">
+          <div className="flex mt-2 items-center justify-center">
 
             {isAuth && firstChat && (
               <>
