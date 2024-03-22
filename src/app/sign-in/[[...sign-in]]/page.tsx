5ad7b4c7dde4cc6b28507185b1579c72ccc42807
Aviral <soniaviral14@gmail.com>
2024-03-22 22:54:52 +0530

    fix

--- src/app/sign-in/[[...sign-in]]/page.tsx
+++ src/app/sign-in/[[...sign-in]]/page.tsx
@@ -7,7 +7,7 @@ export default function Page() {
 
 
             <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
-                <Link href="sign-up" className=" gradient-text-dis"> NO Account? SIGN-UP</Link>
+
                 <SignIn /></div>
         </>
 
--- src/app/sign-up/[[...sign-up]]/page.tsx
+++ src/app/sign-up/[[...sign-up]]/page.tsx
@@ -9,7 +9,7 @@ export default function Page() {
         <>
 
             <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
-                <Link href="sign-in" className=" gradient-text-dis"> Have Account? SIGN-IN</Link>
+
                 <SignUp /></div>
 
         </>
